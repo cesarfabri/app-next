0fcb5c7db09821b17b7e838311cf5c8b66ed30f9
Cesar Fabri <cesar.fabrii@gmail.com>
2022-01-31 18:26:38 -0300

    Implementation index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Button, Flex, Input, Stack, FormLabel, FormControl } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
@@ -7,7 +8,58 @@ const Home: NextPage = () => {
       <Head>
         <title>DI Adm</title>
       </Head>
-      <h1>Teste</h1>
+      <Flex
+        width="100vw"
+        h="100vh"
+        align="center"
+        justify="center"
+      >
+        <Flex
+          as="form"
+          width="100%"
+          maxWidth={360}
+          bg="gray.800"
+          p="8"
+          borderRadius={8}
+          flexDirection="column"
+        >
+          <Stack spacing="4">
+            <FormControl>
+              <FormLabel htmlFor='email'>Email</FormLabel>
+                <Input
+                  name='email'
+                  id='email'
+                  type="email"
+                  focusBorderColor="pink.500"
+                  bgColor="gray.900"
+                  variant="filled"
+                  _hover={{
+                    bgColor:"gray.900"
+                  }}
+                  size="lg"
+                />
+            </FormControl>
+            
+            <FormControl>
+              <FormLabel htmlFor='password'>Senha</FormLabel>
+                <Input
+                  name='password'
+                  id='password'
+                  type="password"
+                  focusBorderColor="pink.500"
+                  bgColor="gray.900"
+                  variant="filled"
+                  _hover={{
+                    bgColor:"gray.900"
+                  }}
+                  size="lg"
+                />
+            </FormControl>
+          </Stack>
+
+          <Button type='submit' mt="6" colorScheme="pink">Entrar</Button>
+        </Flex>
+      </Flex>
     </>
   )
 }
